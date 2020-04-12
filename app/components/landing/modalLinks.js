e8198e16f7d09b14c7222623e83b09a534b83482
Ethan <ebonsignori@gmail.com>
2020-04-12 18:08:26 -0400

    remove PropTypes, function

--- app/components/landing/modalLinks.js
+++ app/components/landing/modalLinks.js
@@ -1,24 +1,8 @@
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 
-const LandingModalLinks = (props) => (
-  <LinkWrapper>
-    {props.children}
-  </LinkWrapper>
-);
-
-const LinkWrapper = styled.div`
+export default styled.div`
   display: flex;
   flex-wrap: wrap;
   flex-direction: column;
   align-items: center;
 `;
-
-LandingModalLinks.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-  ]),
-};
-
-export default LandingModalLinks;
