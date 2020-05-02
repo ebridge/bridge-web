5d7c42d90d5f9b24fa0a1db9080aec8cb8691501
Ethan <ebonsignori@gmail.com>
2020-05-02 11:07:47 -0400

    refactor Layout

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,17 +1,53 @@
 import styled from 'styled-components';
 import Navbar from './Navbar';
+import Bridge from './bridge/Bridge';
+import { breakpoints } from '../lib/styleUtils';
+import Chat from './Chat';
 
-const Layout = styled.div`
-  margin: 20;
-  padding: 20;
-  border: 1px solid #DDD;
-`;
-
-const withLayout = (Page) => () => (
-  <Layout>
+const Layout = () => (
+  <>
     <Navbar />
-    <Page />
-  </Layout>
+    <FlexWrapper>
+      <ContentWrapper>
+        <Bridge />
+      </ContentWrapper>
+      <Chat />
+    </FlexWrapper>
+    <Footer>
+      <h1>Footer</h1>
+    </Footer>
+  </>
 );
 
-export default withLayout;
+const FlexWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  ${breakpoints.mobile} {
+    flex-direction: column;
+  }
+`;
+
+const ContentWrapper = styled.div`
+  background: red;
+  
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 80vw;
+  
+  max-height: 90vh;
+  overflow: auto;
+
+  ${breakpoints.mobile} {
+    width: 100vw;
+    max-height: 50vh;
+  }
+`;
+
+const Footer = styled.div`
+  display: flex;
+  width: 100vw;
+  background: purple;
+`;
+
+export default Layout;
