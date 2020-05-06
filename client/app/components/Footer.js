31ce259c225212b1c68898b5790355804d995645
Ethan <ebonsignori@gmail.com>
2020-05-06 11:12:24 -0400

    add Footer component

---
+++ client/app/components/Footer.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  min-height: ${props => props.height};
+  font-family: ${props => props.theme.fonts.quicksand};
+  background: ${props => props.theme.colors.blue};
+  padding: ${props => `${props.theme.padding.topAndBottom} ${props.theme.padding.leftAndRight}`};
+`;
