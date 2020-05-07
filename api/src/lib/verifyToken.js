edbbda0eead410fa8175b9f89932e0fa147a2c26
Ethan <ebonsignori@gmail.com>
2020-05-07 17:11:38 -0400

    create user routes

---
+++ api/src/lib/verifyToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return res.status(403).send({ auth: false, message: 'No token provided.' });
+  }
+
+  return jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    }
+    req.userId = decoded.id;
+    return next();
+  });
+};
+
+module.exports = verifyToken;
