2608fd3777d4df1c08113154bc5fa3b31b00220a
EthanB <ebonsignori@gmail.com>
2020-06-01 13:48:22 -0400

    handle no header first

--- api/src/rest/middleware/isAuthenticated.js
+++ api/src/rest/middleware/isAuthenticated.js
@@ -12,22 +12,23 @@ const isAuthenticated = (req, res, next) => {
     return next();
   };
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-    return jwt.verify(token, process.env.JWT_SECRET, async (error, decodedToken) => {
-      if (error) {
-        return handleError('Invalid token');
-      }
-      // Compare token expiry (seconds) to current time (in ms) - bail out if token has expired
-      if (decodedToken.exp <= Date.now() / 1000) {
-        return handleError('Expired token');
-      }
-      const [user] = await knex.from(USERS).select('*').where({ id: decodedToken.id });
-      req.user = user;
-      return next();
-    });
+  if (!authHeader) {
+    return handleError('No authorization header on request');
   }
-  return handleError('No authorization header on request');
+
+  const token = authHeader.split(' ')[1];
+  return jwt.verify(token, process.env.JWT_SECRET, async (error, decodedToken) => {
+    if (error) {
+      return handleError('Invalid token');
+    }
+    // Compare token expiry (seconds) to current time (in ms) - bail out if token has expired
+    if (decodedToken.exp <= Date.now() / 1000) {
+      return handleError('Expired token');
+    }
+    const [user] = await knex.from(USERS).select('*').where({ id: decodedToken.id });
+    req.user = user;
+    return next();
+  });
 };
 
 module.exports = isAuthenticated;
