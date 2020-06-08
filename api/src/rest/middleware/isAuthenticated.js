13d3a624621d1298e89b4de617fa24de202abf47
EthanB <ebonsignori@gmail.com>
2020-06-08 14:56:18 -0400

    res.end() instead of error to not pollute logger use user select constant

--- api/src/rest/middleware/isAuthenticated.js
+++ api/src/rest/middleware/isAuthenticated.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const knex = require('../../postgres/knex').getKnex();
 const { USERS } = require('../../lib/constants/tables');
+const { USER_SELECTS } = require('../../lib/constants/selects');
 const logger = require('../../lib/logger')(module);
 const { UnauthorizedError } = require('../../lib/errors');
 
@@ -13,7 +14,7 @@ const isAuthenticated = (req, res, next) => {
   };
 
   if (!authHeader) {
-    return handleError('No authorization header on request');
+    return res.end();
   }
 
   const token = authHeader.split(' ')[1];
@@ -23,9 +24,11 @@ const isAuthenticated = (req, res, next) => {
     }
     // Compare token expiry (seconds) to current time (in ms) - bail out if token has expired
     if (decodedToken.exp <= Date.now() / 1000) {
-      return handleError('Expired token');
+      return res.end();
     }
-    const [user] = await knex.from(USERS).select('*').where({ id: decodedToken.id });
+    const [user] = await knex(USERS)
+      .select(USER_SELECTS)
+      .where({ id: decodedToken.id });
     req.user = user;
     return next();
   });
