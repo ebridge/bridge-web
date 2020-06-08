9f3e7c6ff2cf61be6026b846da2fd4e311c9edfb
EthanB <ebonsignori@gmail.com>
2020-06-08 14:55:41 -0400

    clean up users file,    *remove unecessary .from()    *consistent errors    *consistent spacing and newlines    *use user selects constant

--- api/src/rest/controllers/users.js
+++ api/src/rest/controllers/users.js
@@ -6,6 +6,7 @@ const knex = require('../../postgres/knex').getKnex();
 const isAuthenticated = require('../middleware/isAuthenticated');
 const signJWToken = require('../../lib/token');
 const { USERS } = require('../../lib/constants/tables');
+const { USER_SELECTS } = require('../../lib/constants/selects');
 const {
   ServerError,
   UnauthorizedError,
@@ -28,18 +29,23 @@ router.get('/authenticate', isAuthenticated, async (req, res, next) => {
 router.get('/:displayName', async (req, res, next) => {
   const { displayName } = req.params;
   if (!displayName) {
-    return next(new NotFoundError('No displayName in query.', 'Unable to locate that user.'));
+    return next(new NotFoundError(
+      'No displayName in query.',
+      'Unable to locate that user.'
+    ));
   }
   try {
-    const [user] = await knex(USERS).select(
-      'display_name',
-      'bio',
-      'created_at',
-      'is_banned',
-      'banned_at'
-    ).whereRaw('LOWER(display_name) LIKE \'%\' || LOWER(?) || \'%\' ', displayName.toLowerCase());
+    const [user] = await knex(USERS)
+      .select(USER_SELECTS)
+      .whereRaw(
+        'LOWER(display_name) LIKE \'%\' || LOWER(?) || \'%\' ',
+        displayName.toLowerCase()
+      );
     if (!user) {
-      return next(new NotFoundError('No user found with that displayName.', 'Unable to locate that user.'));
+      return next(new NotFoundError(
+        'No user found with that displayName.',
+        'Unable to locate that user.'
+      ));
     }
     return res.status(200).json({ user });
   } catch (error) {
@@ -53,32 +59,47 @@ router.get('/logout', (req, res) => res.status(200).json({ token: null }));
 router.post('/register', async (req, res, next) => {
   const { email, displayName, password } = req.body;
   if (!email || !displayName || !password) {
-    return next(new UnauthorizedError('Email, display name and password are required to register new user.'));
+    return next(new UnauthorizedError(
+      'Email, display name, and password were not passed to /register route.',
+      'Email, display name and password are required to register new user.'
+    ));
   }
   // Check for duplicate user email
-  const [duplicateUserEmail] = await knex
-    .from(USERS)
+  const [duplicateUserEmail] = await knex(USERS)
     .select('email')
-    .whereRaw('LOWER(email) LIKE \'%\' || LOWER(?) || \'%\' ', email.toLowerCase());
+    .whereRaw(
+      'LOWER(email) LIKE \'%\' || LOWER(?) || \'%\' ',
+      email.toLowerCase()
+    );
   if (duplicateUserEmail) {
-    return next(new ConflictError('Duplicate email found.', 'Email address is already in use.'));
+    return next(new ConflictError(
+      'Duplicate email found.',
+      'Email address is already in use.'
+    ));
   }
   // Check for duplicate user display name
-  const [duplicateUserDisplayName] = await knex
-    .from(USERS)
+  const [duplicateUserDisplayName] = await knex(USERS)
     .select('display_name')
-    .whereRaw('LOWER(display_name) LIKE \'%\' || LOWER(?) || \'%\' ', displayName.toLowerCase());
+    .whereRaw(
+      'LOWER(display_name) LIKE \'%\' || LOWER(?) || \'%\' ',
+      displayName.toLowerCase()
+    );
   if (duplicateUserDisplayName) {
-    return next(new ConflictError('Duplicate display name found.', 'Display name is already in use.'));
+    return next(new ConflictError(
+      'Duplicate display name found.',
+      'Display name is already in use.'
+    ));
   }
   const hashedPassword = bcrypt.hashSync(password, 8);
   try {
-    const [userId] = await knex(USERS).insert({
-      id: uuidv4(),
-      email,
-      display_name: displayName,
-      password_hash: hashedPassword,
-    }).returning('id');
+    const [userId] = await knex(USERS)
+      .insert({
+        id: uuidv4(),
+        email,
+        display_name: displayName,
+        password_hash: hashedPassword,
+      })
+      .returning('id');
     // create and sign token
     const token = signJWToken(userId);
     return res.status(200).json({ displayName, token });
@@ -91,26 +112,32 @@ router.post('/register', async (req, res, next) => {
 router.post('/login', async (req, res, next) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    next(new UnauthorizedError('Email and password are required to login.'));
+    next(new UnauthorizedError(
+      'Email and password were not passed to /login route.',
+      'Email and password are required to login.'
+    ));
   }
   try {
-    const [user] = await knex.from(USERS).select('*').where({ email });
+    const [user] = await knex(USERS)
+      .select('*')
+      .where({ email });
     if (!user) {
-      return next(new UnauthorizedError('No user found with passed email.', 'No account exists with that email.'));
+      return next(new UnauthorizedError(
+        'No user found with passed email.',
+        'No account exists with that email.'
+      ));
     }
     const passwordIsValid = bcrypt.compareSync(password, user.password_hash);
     if (!passwordIsValid) {
-      return next(new UnauthorizedError('Incorrect password for user.', 'Invalid email or password.'));
+      return next(new UnauthorizedError(
+        'Incorrect password for user.',
+        'Invalid email or password.'
+      ));
     }
     const { id } = user;
     const displayName = user.display_name;
     const token = signJWToken(id);
-    return res
-      .status(200)
-      .json({
-        displayName,
-        token,
-      });
+    return res.status(200).json({ displayName, token });
   } catch (error) {
     logger.error(error);
     return next(new ServerError());
@@ -126,7 +153,7 @@ router.post('/login', async (req, res, next) => {
 //     });
 //   }
 //   try {
-//     const [user] = await knex.from(USERS).select('id', 'email_confirmed').where({ email });
+//     const [user] = await knex(USERS).select('id', 'email_confirmed').where({ email });
 //     if (!user) {
 //       return res.status(404).json({
 //         error: 'No user was found with that email',
