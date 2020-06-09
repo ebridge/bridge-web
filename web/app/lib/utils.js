af5d73e2bab3f307161f5bf9e518094f9978150a
EthanB <ebonsignori@gmail.com>
2020-06-09 12:16:59 -0400

    add objKeysToCamel helper func

--- web/app/lib/utils.js
+++ web/app/lib/utils.js
@@ -1,3 +1,5 @@
+import logger from './logger';
+
 export function isIterable(obj) {
   if (obj == null) {
     return false;
@@ -10,4 +12,15 @@ export function screamingToCamel(screamingString) {
   return snakeString.replace(/([-_][a-z])/ig, (newWordFirstLetter) => newWordFirstLetter.toUpperCase()
     .replace('-', '')
     .replace('_', ''));
+}
+
+export function objKeysToCamel(objOrValue) {
+  if (!objOrValue) return logger.warn('No object or value passed to objKeysToCamel');
+  if (typeof objOrValue !== 'object') return objOrValue;
+  const camelObj = {};
+  Object.keys(objOrValue).forEach(key => {
+    camelObj[screamingToCamel(key)] = objKeysToCamel(objOrValue[key]);
+  });
+
+  return camelObj;
 }
