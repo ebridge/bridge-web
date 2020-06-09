2c4e5ce54ad9f53f155696c193d7aabde1e14c29
EthanB <ebonsignori@gmail.com>
2020-06-09 12:17:27 -0400

    add user profile fetching, updating bio

--- web/app/pages/user/[displayName].js
+++ web/app/pages/user/[displayName].js
@@ -1,26 +1,91 @@
+import { useState } from 'react';
 import { connect } from 'react-redux';
 import { fromJS } from 'immutable';
-import { userGetProfile, userUpdateProfile } from '../../redux/actions/userActions';
+import { getRequest } from '../../redux/service';
+import { userUpdateProfile } from '../../redux/actions/userActions';
+import { objKeysToCamel } from '../../lib/utils';
 
-const Profile = ({ profile }) => <p>Profile: {profile}</p>;
+const Profile = ({
+  userProfile,
+  error,
+  dispatchUserUpdateProfile,
+}) => {
+  const [profile, setUserProfile] = useState(userProfile);
+  const [editedProfile, setEditedProfile] = useState(userProfile);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleChange = evt => {
+    const { name, value } = evt.target;
+    setEditedProfile(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSave = async () => {
+    const updatedProfile = await dispatchUserUpdateProfile(userProfile.displayName, editedProfile);
+    setUserProfile(updatedProfile);
+    return setIsEditing(false);
+  };
+
+  const {
+    displayName,
+    bio,
+    createdAt,
+    updatedAt,
+    isBanned,
+    bannedAt,
+  } = profile;
+  // TODO: Style profile and display profile data
+  return (
+    <>
+      <div>
+        <h1>User Profile</h1>
+        <ul>
+          <li>Display Name: {displayName}</li>
+          <li>Created: {createdAt}</li>
+          <li>Updated: {updatedAt}</li>
+          {!isBanned ? <li> This user is banned! They were banned: {bannedAt}</li> : null}
+          {isEditing
+            ? <li>
+              <textarea name='bio' value={editedProfile.bio || ''} onChange={handleChange}></textarea>
+              <button onClick={handleSave}>Save</button>
+            </li>
+            : <li>Bio: {bio || 'There\'s nothing here.'} <button onClick={() => setIsEditing(true)}>Edit</button></li>}
+          {error && <li>{error}</li>}
+        </ul>
+      </div>
+    </>
+  );
+};
 
 Profile.getInitialProps = async (ctx) => {
   const { displayName } = ctx.query;
-  userGetProfile(displayName);
+  const response = await getRequest(`/users/${displayName}`);
+  if (response.error) {
+    return {
+      error: response?.error,
+      userProfile: null,
+    };
+  }
+  const { user } = response;
+  const userProfile = objKeysToCamel(user);
+  return {
+    error: null,
+    userProfile,
+  };
 };
 
 const mapStateToProps = (state = fromJS({})) => {
-  const api = state.get('api');
   const user = state.get('user');
   return {
-    apiError: api.get('userGetProfileState').error,
-    profile: user.get('profile'),
+    updatedProfile: user.get('updatedProfile'),
   };
 };
 
 const mapDispatchToProps = dispatch => ({
-  dispatchUserUpdateProfile: (profile) => dispatch(
-    userUpdateProfile(profile)
+  dispatchUserUpdateProfile: (displayName, profile) => dispatch(
+    userUpdateProfile(displayName, profile)
   ),
 });
 
