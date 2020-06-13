7dcf3f6255d386e1818da969d57e1477145c42c7
Ethan <ebonsignori@gmail.com>
2020-06-13 00:13:36 -0400

    add ModalCheckbox

---
+++ web/app/components/modals/common/ModalCheckbox.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+const Checkbox = ({
+  inputType,
+  type,
+  onChange,
+  checked,
+  label,
+}) => (
+  <CheckboxLabel>
+    <CheckboxInput type={type} onChange={(event) => onChange(inputType, event.target.checked)} checked={checked} />
+    <span>{label}</span>
+  </CheckboxLabel>
+);
+
+const CheckboxLabel = styled.label`
+  display: inline-flex;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+`;
+
+const CheckboxInput = styled.input`
+  cursor: pointer;
+  height: 25px;
+  width: 25px;
+
+  /* Appearance */
+  -webkit-appearance: none;
+     -moz-appearance: none;
+       -o-appearance: none;
+          appearance: none;
+
+  /* Border */
+  border: 1px solid #000;
+  border-radius: 4px;
+  
+  background-color: #fff;
+  transition-duration: 0.3s;
+
+  &:checked {
+    border: 1px solid ${props => props.theme.colors.buttonGreen};
+    background-color: ${props => props.theme.colors.buttonGreen};
+  }
+
+  &:checked:hover {
+    background-color: ${props => props.theme.colors.buttonGreenHover};
+  }
+
+  &:checked + span::before {
+    content: '\u2713'; /* checkmark */
+    display: block;
+    text-align: center;
+    color: #fff;
+    position: absolute;
+    left: 0.7rem;
+    top: 0.3rem;
+  }
+`;
+
+export default Checkbox;
