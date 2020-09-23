535ebd21eb80c2e72269b9dd025395c8b56f1247
Ethan <ebonsignori@gmail.com>
2020-09-23 18:07:26 -0400

    export app

--- api/src/server.js
+++ api/src/server.js
@@ -11,13 +11,16 @@ const { initializeKnex } = require('./postgres/knex');
 
 logger.info('Initalizing server.', { NODE_ENV: process.env.NODE_ENV });
 
+let listeningApp;
+const app = express();
+const getAsyncExports = () => ({ listeningApp });
+
 // Use an async entry point
 async function entryPoint() {
   // Initialize postgres query builder, Knex
   await initializeKnex();
 
   // Initialize Express server
-  const app = express();
   app.use(cors());
   app.use(bodyParser.json());
   // eslint-disable-next-line global-require
@@ -25,10 +28,21 @@ async function entryPoint() {
   app.use(errorHandler);
 
   // Start Server
-  const port = process.env.API_PORT || 3001;
+  let port = process.env.API_PORT || 3001;
+  if (process.NODE_ENV === 'test') {
+    port = process.env.TEST_API_PORT;
+  }
   const server = http.createServer(app);
-  server.listen(port, () => logger.info(`Server up and running on port ${port}`));
+  listeningApp = server.listen(port, () => {
+    app.emit('started');
+    logger.info(`Server up and running on port ${port}`);
+  });
 }
 
 // Start server
 entryPoint();
+
+module.exports = {
+  app,
+  getAsyncExports,
+};
