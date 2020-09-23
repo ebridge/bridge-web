1f6101abc9e2a752e2936ad5f2c24be36fca085a
Ethan <ebonsignori@gmail.com>
2020-09-23 18:15:33 -0400

    switch to fakeUser

--- api/tests/userTests.js
+++ api/tests/userTests.js
@@ -1,19 +1,21 @@
 const { expect } = require('chai');
 const request = require('supertest');
+const faker = require('faker');
 const expressApp = require('../src/server').app;
 
 const agent = request.agent(expressApp);
+const fakeUser = {
+  email: faker.name.findName(),
+  password: faker.internet.password(),
+  displayName: faker.internet.userName(),
+}
 
 describe('/rest/users', () => {
   describe('POST /register', () => {
     it('should register a new user', async () => {
       const result = await agent
         .post('/rest/users/register')
-        .send({
-          email: 'fake@fakedomain.com',
-          password: '1234',
-          displayName: 'FakeUser',
-        });
+        .send(fakeUser);
     });
   });
 });
