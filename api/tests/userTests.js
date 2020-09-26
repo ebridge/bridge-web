da88544ecfa359bff12fe7c19312d66b7827e87f
Ethan <ebonsignori@gmail.com>
2020-09-25 23:40:37 -0400

    add /authenticate test, expects

--- api/tests/userTests.js
+++ api/tests/userTests.js
@@ -9,26 +9,47 @@ const fakeUser = {
   password: faker.internet.password(),
   displayName: faker.internet.userName(),
 }
+let token;
 
 describe('/rest/users', () => {
   // Register
   describe('POST /register', () => {
-    it('should register a new user', async () => {
+    it('should register a new user', async done => {
       const result = await agent
         .post('/rest/users/register')
         .send(fakeUser);
+      expect(result).status(200);
+      expect(result.error).to.be(null);
+      done();
     });
   });
 
   // Login
   describe('POST /login', () => {
-    it('should login the newly registered user', async () => {
+    it('should login the newly registered user', async done => {
       const result = await agent
         .post('/rest/users/login')
         .send({
           email: fakeUser.email,
           password: fakeUser.password,
         })
+        expect(result).status(200);
+        token = result.token;
+        done();
+    })
+  })
+
+  // Authenitcate
+  describe('GET /authenticate', () => {
+    it('should authenticate the logged in user', async done => {
+      const result = await agent
+        .get('/rest/users/authenticate')
+        .set({ Authorization: `Bearer ${token}`})
+      expect(result).status(200);
+      expect(result).should.be.an('object');
+      expect(result).should.have.property('displayName');
+      expect(result.displayName).to.equal(fakeUser.displayName);
+      done()
     })
   })
 });
