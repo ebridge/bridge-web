1ea13877d86f1684a75fdf02594dcd279a24371e
Ethan Bonsignori <ebonsignori@gmail.com>
2020-10-12 12:19:29 -0400

    add removeTestUser func

--- api/tests/testUtils.js
+++ api/tests/testUtils.js
@@ -1,13 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const faker = require('faker');
+const { getKnex } = require('../src/postgres/knex');
+const { USERS } = require('../src/lib/constants/tables');
+const logger = require('../src/lib/logger');
 
-const generateFakeUser = () => ({
-  email: faker.name.findName(),
+const generateTestUser = () => ({
+  email: faker.internet.email(),
   password: faker.internet.password(),
   displayName: faker.internet.userName(),
 });
 
+const removeTestUser = email => {
+  const knex = getKnex();
+  if (!email) {
+    return logger.error('No email passed to removeTestUser.');
+  }
+  return knex(USERS).where('email', email).del();
+};
 
 module.exports = {
-  generateFakeUser,
+  generateTestUser,
+  removeTestUser,
 };
