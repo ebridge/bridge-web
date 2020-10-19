fe4b709642ec327d9f2c52e0783296aef2d84c08
Ethan Bonsignori <ebonsignori@gmail.com>
2020-10-19 08:50:04 -0400

    add generate/remove test rooms

--- api/tests/testUtils.js
+++ api/tests/testUtils.js
@@ -1,8 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const faker = require('faker');
+const { v4: uuidv4 } = require('uuid');
 const { getKnex } = require('../src/postgres/knex');
-const { USERS } = require('../src/lib/constants/tables');
 const logger = require('../src/lib/logger');
+const {
+  USERS,
+  ROOMS,
+} = require('../src/lib/constants/tables');
+
+const knex = getKnex();
 
 const generateTestUser = () => ({
   email: faker.internet.email(),
@@ -11,14 +17,47 @@ const generateTestUser = () => ({
 });
 
 const removeTestUser = email => {
-  const knex = getKnex();
   if (!email) {
     return logger.error('No email passed to removeTestUser.');
   }
-  return knex(USERS).where('email', email).del();
+  try {
+    return knex(USERS).where('email', email).del();
+  } catch (error) {
+    return logger.error(error);
+  }
+};
+
+const generateTestRooms = numberOfRooms => {
+  if (!numberOfRooms) {
+    return logger.error('No numberOfRooms passed to generateTestRooms');
+  }
+  // create array of room objects for knex insert
+  const roomsArr = [];
+  for (let i = 1; i <= numberOfRooms; i += 1) {
+    const roomToBeAdded = {
+      id: uuidv4(),
+      room_number: i,
+    };
+    roomsArr.push(roomToBeAdded);
+  }
+  try {
+    return knex(ROOMS).insert(roomsArr);
+  } catch (error) {
+    return logger.error(error);
+  }
+};
+
+const removeTestRooms = numberOfRooms => {
+  try {
+    return knex(ROOMS).whereBetween('room_number', [1, numberOfRooms]).del();
+  } catch (error) {
+    return logger.error(error);
+  }
 };
 
 module.exports = {
   generateTestUser,
   removeTestUser,
+  generateTestRooms,
+  removeTestRooms,
 };
