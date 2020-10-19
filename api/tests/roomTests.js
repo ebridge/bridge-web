2cbedc72146e5cdbb34e91f1b77210286aa4baf7
Ethan Bonsignori <ebonsignori@gmail.com>
2020-10-19 15:49:50 -0400

    add GET /rooms test

--- api/tests/roomTests.js
+++ api/tests/roomTests.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const { expect } = require('chai');
 const request = require('supertest');
 const expressApp = require('../src/server').app;
@@ -10,10 +9,26 @@ const {
 const agent = request.agent(expressApp);
 const NUMBER_OF_ROOMS = 5;
 
+
 describe('/rest/rooms', () => {
   // create test rooms
-  generateTestRooms(NUMBER_OF_ROOMS);
+  before(async () => {
+    await generateTestRooms(NUMBER_OF_ROOMS);
+  });
+
+  describe('GET', async () => {
+    it('should return an array of all rooms', async () => {
+      const result = await agent
+        .get('/rest/rooms');
+      expect(result.status).to.equal(200);
+      expect(result.error).to.equal(false);
+      expect(result.body).to.be.an('array');
+      expect(result.body).to.have.lengthOf(NUMBER_OF_ROOMS);
+    });
+  });
 
   // remove test rooms
-  removeTestRooms(NUMBER_OF_ROOMS);
+  after(async () => {
+    await removeTestRooms(NUMBER_OF_ROOMS);
+  });
 });
