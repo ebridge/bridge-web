27a8879b40c2623883d19d0ae16a749deb427fa1
Ethan Bonsignori <ebonsignori@gmail.com>
2020-10-19 18:17:58 -0400

    add testLogger

---
+++ api/tests/testLogger.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+module.exports = {
+  log: message => {
+    console.log(message);
+  },
+  warn: message => {
+    console.warn(message);
+  },
+  error: message => {
+    console.error(message);
+  },
+};
