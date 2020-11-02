24f202d5c18b80f3b4cdc22ad69887068ac1dc0c
Ethan Bonsignori <ebonsignori@gmail.com>
2020-11-02 12:20:02 -0500

    add seat enum property to join table

--- api/src/postgres/migrations/20201012122310_join_users_and_rooms.js
+++ api/src/postgres/migrations/20201012122310_join_users_and_rooms.js
@@ -4,6 +4,7 @@ const { USERS, ROOMS } = require('../../lib/constants/tables');
 
 exports.up = (knex) => knex.schema.createTable(JOIN_USERS_AND_ROOMS, (table) => {
   table.uuid('id').primary();
+  table.enu('seat', ['N', 'E', 'S', 'W']).notNull();
 
   // Join users table
   const USER_ID = 'user_id';
@@ -16,6 +17,7 @@ exports.up = (knex) => knex.schema.createTable(JOIN_USERS_AND_ROOMS, (table) => 
   table.foreign(ROOM_ID).references(`${ROOMS}.id`);
 
   table.unique([USER_ID, ROOM_ID]);
+  table.unique(['seat', ROOM_ID]);
 });
 
 exports.down = (knex) => knex.schema.dropTable(JOIN_USERS_AND_ROOMS);
