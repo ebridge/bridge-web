09703c78d9b692d1ec4662d935aa3d63793218d8
Ethan Bonsignori <ebonsignori@gmail.com>
2020-11-02 12:25:13 -0500

    move Chat.js, add ability to toggle position

---
+++ web/app/components/chat/Chat.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from 'styled-components';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import { breakpoints } from '../../lib/styleUtils';
+import ChatInput from './ChatInput';
+// import { submitForm } from '../redux/actions/formActions';
+// import { TEXTAREA } from '../constants/formConstants';
+
+class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    const {
+      width,
+      chatPosition,
+      setChatPosition,
+    } = this.props;
+    return (
+      <ChatWrapper width={width}>
+        <ChatBanner chatPosition={chatPosition}>
+          <button onClick={setChatPosition}>
+            {chatPosition === 'right'
+              ? <FirstPage />
+              : <LastPage />
+            }
+          </button>
+        </ChatBanner>
+        <ChatContainer />
+        <ChatInput
+          type='textarea'
+          placeholder='Type a message...'
+        />
+      </ChatWrapper>
+    );
+  }
+}
+
+const ChatWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: ${props => props.width};
+  background: ${props => props.theme.colors.lightBlue};
+
+  ${breakpoints.mobile} {
+    width: 100vw;
+  }
+`;
+
+const ChatBanner = styled.div`
+  display: flex;
+  flex-direction: ${({ chatPosition }) => (chatPosition === 'right' ? 'row' : 'row-reverse')};
+  position: absolute;
+  top: 0;
+
+  width: 100%;
+  height: 30px;
+  background: #fff;
+
+`;
+
+const ChatContainer = styled.div`
+  display: flex;
+  background: ${props => props.theme.colors.lightBlue};
+  height: 100%;
+  width: 98%;
+`;
+
+export default Chat;
