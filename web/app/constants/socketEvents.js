d8f9b18e7fc916737f58060748a75abed1142adb
Ethan Bonsignori <ebonsignori@gmail.com>
2020-11-04 10:18:52 -0500

    add socket events

---
+++ web/app/constants/socketEvents.js
@@ -0,0 +1,10 @@
+export const WS_GLOBAL_MESSAGES = 'global_messages';
+export const WS_GLOBAL_MESSAGE = 'global_message';
+export const WS_ROOM_MESSAGES = 'room_messages';
+export const WS_ROOM_MESSAGE = 'room_message';
+export const WS_ALL_EVENTS = {
+  WS_GLOBAL_MESSAGES,
+  WS_GLOBAL_MESSAGE,
+  WS_ROOM_MESSAGES,
+  WS_ROOM_MESSAGE,
+};
