c4216ad86d1b8662b9d927256934a5447506aaac
Ethan Bonsignori <ebonsignori@gmail.com>
2020-11-11 12:08:11 -0500

    Refactor footer

--- web/app/components/Footer.js
+++ web/app/components/Footer.js
@@ -1,12 +1,21 @@
 import styled from 'styled-components';
 
-export default styled.div`
+const Footer = ({ height }) => (
+  <StyledFooter height={height}>
+    <span>&copy; Copyright Ethan Bonsignori 2020</span>
+  </StyledFooter>
+);
+
+const StyledFooter = styled.footer`
   display: flex;
+  flex-shrink: 0;
   justify-content: center;
   align-items: center;
   width: 100vw;
-  min-height: ${props => props.height};
+  min-height: ${props => props.height || '5vh'};
   font-family: ${props => props.theme.fonts.quicksand};
-  background: ${props => props.theme.colors.blue};
+  background: ${props => props.theme.colors.mainGrey};
   padding: ${props => `${props.theme.padding.topAndBottom} ${props.theme.padding.leftAndRight}`};
 `;
+
+export default Footer;
