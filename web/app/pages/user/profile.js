9c529342a00721e1cac809240e4f84ea5c88317d
Ethan Bonsignori <ebonsignori@gmail.com>
2020-11-16 15:33:08 -0500

    add profile picture (placeholder)

--- web/app/pages/user/profile.js
+++ web/app/pages/user/profile.js
@@ -1,8 +1,46 @@
+import styled from 'styled-components';
+import EditIcon from '@material-ui/icons/Edit';
+import PageWrapper from '../../components/common/PageWrapper';
 
 const Profile = ({
   displayName,
 }) => (
-  <div>{displayName}</div>
+  <PageWrapper displayName={displayName}>
+    <h1>Your Profile</h1>
+    <ProfilePictureContainer>
+      Profile Picture
+      <EditIconWrapper>
+        <EditIcon /> Edit
+      </EditIconWrapper>
+      <ProfilePicture />
+    </ProfilePictureContainer>
+  </PageWrapper>
 );
+
+const ProfilePictureContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+const ProfilePicture = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background: grey;
+`;
+
+const EditIconWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+
+  display: flex;
+  align-items: center;
+  padding: 3px 6px;
+
+  background: white;
+  border: 1px solid grey;
+  border-radius: 6px;
+`;
 
 export default Profile;
