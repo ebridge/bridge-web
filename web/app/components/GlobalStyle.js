acacc61034d78db1d4d4dd8e885decb81283100d
EthanBonsignori <ebonsignori@gmail.com>
2020-12-15 12:13:22 -0500

    add react-datepicker

--- web/app/components/GlobalStyle.js
+++ web/app/components/GlobalStyle.js
@@ -15,6 +15,23 @@ const GlobalStyle = createGlobalStyle`
   *:after {
     box-sizing: border-box;
   }
+
+  textarea, input, select {
+    font-family: sans-serif;
+  }
+
+  .react-datepicker__input-container input {
+    color: #384047;
+    background-color: #e8eeef;
+    box-shadow: 0px 1px 1px rgba(0,0,0,0.03) inset;
+    border-radius: 4px;
+    width: 100%;
+    padding: 1em;
+    border: 1px solid black;
+  }
+  .react-datepicker-popper {
+    z-index: 9999 !important;
+  }
 `;
 
 export default GlobalStyle;
